8de3a7c962eb7269875b3f2433307f0a8fa58eb5
Awardz <reawardz@gmail.com>
2025-03-13 21:53:27 -0400

    Updated the main menu presentation by Dylan.

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,9 +1,14 @@
-extends VBoxContainer
+extends Node
 
 
 const main = preload("res://scenes/main.tscn")
 const leaderboard = preload("res://scenes/leaderboard.tscn")
+var pivot
+@export var rotation_speed : float = 0.15
 
+func _ready():
+	pivot = $Pivot
+	
 func _on_button_pressed():
 	get_tree().change_scene_to_packed(main) 
 
@@ -12,3 +17,6 @@ func _on_button_2_pressed():
 
 func _on_button_3_pressed():
 	get_tree().quit()
+
+func _physics_process(delta):
+	pivot.global_rotation += Vector3(0, rotation_speed * delta, 0)
